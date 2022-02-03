f28716557f2a851b97aaea2e31911b405e1923bf
Minor G <minorg@server.fake>
2022-02-03 17:40:16 -0500

    test data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# test-data
+# Towndex test data
+
+This directory contains inputs for the `etl` pipeline and corresponding outputs.
+
+The inputs reference copies of documents on `minorgordon.net` rather than the originals, so that the links don't break and there's no test traffic to the origin site.
---
+++ etl.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+cd %~dp0
+for %%I in (.) do set CurrDirName=%%~nxI
+set DEBUG=
+
+cd ..\etl
+CALL poetry run etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName%\documents -o ..\%CurrDirName%\etl-output\json_directory --output-format json_directory
+CALL poetry run etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName%\documents -o ..\%CurrDirName%\etl-output\json_file\data.json --output-format json_file
+cd ..\%CurrDirName%
