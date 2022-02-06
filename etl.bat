555b7677cea595ca14db5242403cd4c3df614698
Minor G <minorg@server.fake>
2022-02-06 11:14:13 -0500

    updating test data

--- etl.bat
+++ etl.bat
@@ -8,3 +8,4 @@ cd ..\etl
 CALL poetry run etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName%\documents -o ..\%CurrDirName%\etl-output\json_directory --output-format json_directory
 CALL poetry run etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName%\documents -o ..\%CurrDirName%\etl-output\json_file\data.json --output-format json_file
 cd ..\%CurrDirName%
+COPY etl-output\json_file\data.json ..\gui\test\testData.json
