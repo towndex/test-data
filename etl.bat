4cdc74bcadffae4ee61ec6481c297f1bf45b43cf
Minor G <minorg@server.fake>
2022-03-09 20:42:05 -0500

    generate test data

--- etl.bat
+++ etl.bat
@@ -5,8 +5,9 @@ for %%I in (.) do set CurrDirName=%%~nxI
 set DEBUG=
 
 pushd ..\cli
-REM CALL poetry run cli etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName%\documents -o ..\%CurrDirName%\etl-output --output-format json_directory
+REM CALL poetry run cli etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName% -o ..\%CurrDirName%\etl-output\json_file_per_model_type --output-format json_file_per_model_type
 REM CALL poetry run cli etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i 1VYy28c7rnwT6pd3OGwfmx63GXrtdGJ8mTm0Fx1oN2ok --input-format google_sheets -o ..\%CurrDirName%\etl-output --output-format json_directory
-CALL poetry run cli etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i 1VYy28c7rnwT6pd3OGwfmx63GXrtdGJ8mTm0Fx1oN2ok --input-format google_sheets -o ..\%CurrDirName%\etl-output\json_file\data.json --output-format json_file
+REM CALL poetry run cli etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i 1VYy28c7rnwT6pd3OGwfmx63GXrtdGJ8mTm0Fx1oN2ok --input-format google_sheets -o ..\%CurrDirName%\etl-output\json_single_file\data.json --output-format json_single_file
+CALL poetry run cli etl --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName% -o ..\%CurrDirName%\..\gui\test\data --output-format json_file_per_model_type
 
 popd
--- gui-watch.bat
+++ gui-watch.bat
@@ -5,5 +5,5 @@ for %%I in (.) do set CurrDirName=%%~nxI
 set DEBUG=
 
 pushd ..\cli
-CALL poetry run cli gui --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName%\documents --input-format json_directory -o ..\%CurrDirName%\gui-output --watch
+CALL poetry run cli gui --cache ..\%CurrDirName%\etl-cache %DEBUG% -i ..\%CurrDirName% -o ..\%CurrDirName%\gui-output --watch
 popd
